04cb7e5ba7cf15d904536dd9161ec98e5c7e0208
Addison Chan <addison95132@gmail.com>
2016-10-04 22:39:57 -0700

    moved main.java -> Main.java

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@
 
 import java.util.Scanner;
 
-public class main {
+public class Main {
     public static void main (String [] args){
         Scanner in = new Scanner(System.in);
         String userInput = in.nextLine().replaceAll("\\s*","");
